4c62cee73e5fe0ba2e60cef5e80acfa93d9543d9
abirAbuAsim <abir.sakif@gmail.com>
2024-06-11 18:00:53 +0600

    Session 10: Added frontend folder

---
+++ session10-budget-tracker-react-node/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BudgetTracker from './BudgetTracker';
+
+// Functional Component
+function App() {
+  return (
+    <div className="bg-gray-100 p-5 min-h-screen">
+      <BudgetTracker/>
+    </div>
+  );
+}
+
+export default App;
